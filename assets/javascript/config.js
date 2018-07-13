9902a64cfb3426cceca70bcaf096823b9ca5307e
steinbrennerGit <steinbrennergit@gmail.com>
2018-07-13 14:45:51 -0700

    test

---
+++ assets/javascript/config.js
@@ -0,0 +1,9 @@
+const fbApiKey = "AIzaSyC2g-lR_OVeTpDZtdpS6gmEMGkdTQckwcE";
+const fbAuthDomain = "steinbrennerprojecttestbench.firebaseapp.com";
+const fbDatabaseURL = "https://steinbrennerprojecttestbench.firebaseio.com";
+const fbProjectId = "steinbrennerprojecttestbench";
+const fbStorageBucket = "steinbrennerprojecttestbench.appspot.com";
+const fbMessagingSenderId = "623159719138";
+
+const zomatoAPI = "284d8bf6da6b7fc3efc07100c1246454";
+const bingAPI = "AhquGMow-8CnsRU8ba3bjzCJHPaeg7xz6sQ47x1-xZuI5fEHPTd105T_za-OXnBQ";
