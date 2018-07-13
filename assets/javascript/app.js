56a995a808b3f25bba245f59580a2159fdbadf4a
steinbrennerGit <steinbrennergit@gmail.com>
2018-07-13 12:04:44 -0700

    changed repo and folder name

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,15 @@
-// found at https://steinbrennergit.github.io/project-one/
-// repo at https://github.com/steinbrennergit/project-one/
+// found at https://steinbrennergit.github.io/welp-project/
+// repo at https://github.com/steinbrennergit/welp-project/
+
+// Initialize Firebase - replace with your own config object!
+var config = {
+    apiKey: "AIzaSyC2g-lR_OVeTpDZtdpS6gmEMGkdTQckwcE",
+    authDomain: "steinbrennerprojecttestbench.firebaseapp.com",
+    databaseURL: "https://steinbrennerprojecttestbench.firebaseio.com",
+    projectId: "steinbrennerprojecttestbench",
+    storageBucket: "steinbrennerprojecttestbench.appspot.com",
+    messagingSenderId: "623159719138"
+};
+firebase.initializeApp(config);
+const db = firebase.database();
+/*******************************/
