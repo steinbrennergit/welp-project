8829e678f22941a8af242a62adba4db916ed1064
steinbrennerGit <steinbrennergit@gmail.com>
2018-07-13 12:39:39 -0700

    removed api key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,12 +3,7 @@
 
 // Initialize Firebase - replace with your own config object!
 var config = {
-    apiKey: "AIzaSyC2g-lR_OVeTpDZtdpS6gmEMGkdTQckwcE",
-    authDomain: "steinbrennerprojecttestbench.firebaseapp.com",
-    databaseURL: "https://steinbrennerprojecttestbench.firebaseio.com",
-    projectId: "steinbrennerprojecttestbench",
-    storageBucket: "steinbrennerprojecttestbench.appspot.com",
-    messagingSenderId: "623159719138"
+    
 };
 firebase.initializeApp(config);
 const db = firebase.database();
