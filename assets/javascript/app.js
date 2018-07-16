1855c883d6c58e85118c372890eed184321d82c4
steinbrennergit <steinbrennergit@gmail.com>
2018-07-15 20:29:57 -0700

    added config file back to repo because it will need to be in there to demo/grade anyway

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,14 +2,6 @@
 // repo at https://github.com/steinbrennergit/welp-project/
 
 // Initialize Firebase - replace with your own config object!
-var fbConfig = {
-    apiKey: fbApiKey,
-    authDomain: fbAuthDomain,
-    databaseURL: fbDatabaseURL,
-    projectId: fbProjectId,
-    storageBucket: fbStorageBucket,
-    messagingSenderId: fbMessagingSenderId
-};
 firebase.initializeApp(fbConfig);
 const db = firebase.database();
 /*******************************/
---
+++ assets/javascript/config.js
@@ -0,0 +1,11 @@
+var fbConfig = {
+    apiKey: "AIzaSyC2g-lR_OVeTpDZtdpS6gmEMGkdTQckwcE",
+    authDomain: "steinbrennerprojecttestbench.firebaseapp.com",
+    databaseURL: "https://steinbrennerprojecttestbench.firebaseio.com",
+    projectId: "steinbrennerprojecttestbench",
+    storageBucket: "steinbrennerprojecttestbench.appspot.com",
+    messagingSenderId: "623159719138"
+};
+
+const zomatoAPI = "284d8bf6da6b7fc3efc07100c1246454";
+const bingAPI = "AhquGMow-8CnsRU8ba3bjzCJHPaeg7xz6sQ47x1-xZuI5fEHPTd105T_za-OXnBQ";
